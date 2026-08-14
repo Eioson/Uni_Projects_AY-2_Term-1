70b68e7b3fa02ab51cfba73f248e923e0b5455e3
Eioson <156309476+Eioson@users.noreply.github.com>
2026-08-14 13:39:10 +0800

    [8/14 @ 1338 hrs] Implemented a doubly linked list

---
+++ Seatwork7_no3.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Node{
+    public int? Data;
+    public Node? Next;
+    public Node? Back;
+    
+    public Node(int data){
+        Back = null;    
+        Data = data;
+        Next = null;
+    } }
+
+public class Program {
+    public static void Main(){
+        Node zero = new Node(0);
+        Node first = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+        
+        first.Back = null; first.Next = second;
+        second.Back = first; second.Next = third;
+        third.Back = second; third.Next = fourth;
+        fourth.Back = third; fourth.Next = fifth;
+        fifth.Back = fourth; fifth.Back = null;
+        
+        Node? current = first;
+        while (current != null){
+            Console.Write(current.Data + (current.Next != null ? " <-> " : ""));
+            current = current.Next; }
+            
+        Console.WriteLine();
+    } }
